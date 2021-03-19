da80069f0e120733e4dac5ea1199a4a86a7ae436
Lyle Rubin <lylerubin912@gmail.com>
2021-03-19 08:54:21 -0800

    Added event listeners

--- events/events.js
+++ events/events.js
@@ -4,5 +4,18 @@ var button = document.querySelector('button')
 button.addEventListener('click', addParagraph)
 
 function addParagraph() {
-  console.log("callback working")
+  if (!document.getElementById("buttonPar")) {
+    let paragraph = document.createElement("p")
+    paragraph.setAttribute("id", "buttonPar")
+    paragraph.appendChild(document.createTextNode("How you doing?"))
+    button.after(paragraph)
+  }
+  else document.getElementById("buttonPar").remove()
+}
+
+document.addEventListener("keypress", callback)
+
+function callback(event){
+  if(event.code ==="Equal") addParagraph()
+  if(event.code ==="KeyX") button.removeEventListener('click', addParagraph)
 }
